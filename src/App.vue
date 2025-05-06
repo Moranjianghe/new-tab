fb29e401dc2512da4fdddce73982cbc25c370c2f
Moranjianghe <xzb38@outlook.com>
2025-05-07 02:27:52 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+<div class="">
+  <!--search bar-->
+  <div>
+    <input
+      v-model="query"
+      @keyup.enter="search"
+      />
+    <button @click="search">Search</button>
+  </div>
+</div>
+</template>
